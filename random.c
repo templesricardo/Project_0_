7aaff77585a968bcc8812710eca53795455abdc0
templesricardo <ricardo.smith@temple.edu>
2026-08-31 01:56:37 -0400

    Add rand_string function to generate random strings
    Implement a function to generate a random string.

--- random.c
+++ random.c
@@ -1,1 +1,13 @@
+#include <stdlib.h>
+#include <stddef.h>
 
+void rand_string(char *s, size_t size)
+{
+    const char characters[] = "abcdefghijklmnopqrstuvwxyz";
+
+    for (size_t i = 0; i < size; i++) {
+        s[i] = characters[rand() % (sizeof(characters) - 1)];
+    }
+
+    s[size] = '\0';
+}
