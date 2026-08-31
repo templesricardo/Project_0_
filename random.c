e04940170eca957e19b4218b20d4124694bce3e1
templesricardo <ricardo.smith@temple.edu>
2026-08-31 02:37:51 -0400

    Refactor random string generation logic

--- random.c
+++ random.c
@@ -13,13 +13,3 @@ void rand_string(char *s, size_t size)
 
     s[size] = '\0';
 }
-=======
-    const char characters[] = "abcdefghijklmnopqrstuvwxyz"; 
-
-    for (size_t i = 0; i < size; i++) {
-        s[i] = characters[rand() % (sizeof(characters) - 1)];
-    }
-
-    s[size] = '\0';
-}
->>>>>>> 751e2d8d6fedcd3a80d544fe1b2fbfa32ba3d298
