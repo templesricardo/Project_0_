746599147201b2dfeab1d6f75658c71bfe906cd5
templesricardo <ricardo.smith@temple.edu>
2026-08-31 02:26:06 -0400

    Add comment to rand_string function
    Updated rand_string function to include a comment.

--- random.c
+++ random.c
@@ -1,9 +1,9 @@
 #include <stdlib.h>
 #include <stddef.h>
 
-void rand_string(char *s, size_t size)
+void rand_string(char *s, size_t size) //make random string
 {
-    const char characters[] = "abcdefghijklmnopqrstuvwxyz";
+    const char characters[] = "abcdefghijklmnopqrstuvwxyz"; 
 
     for (size_t i = 0; i < size; i++) {
         s[i] = characters[rand() % (sizeof(characters) - 1)];
